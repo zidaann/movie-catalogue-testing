a377682c95b6e1b196dcf32bce812e93d339c046
zidaann <jourjizaidan8@gmail.com>
2023-05-13 20:24:26 +0700

    first commit

---
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -0,0 +1,51 @@
+/* eslint-disable class-methods-use-this */
+class FavoriteMovieSearchPresenter {
+  constructor({ favoriteMovies }) {
+    this._listenToSearchRequestByUser();
+    this._favoriteMovies = favoriteMovies;
+  }
+
+  _listenToSearchRequestByUser() {
+    this._queryElement = document.getElementById('query');
+    this._queryElement.addEventListener('change', (event) => {
+      this._searchMovies(event.target.value);
+    });
+  }
+
+  async _searchMovies(latestQuery) {
+    this._latestQuery = latestQuery.trim();
+
+    let foundMovies;
+    if (this.latestQuery.length > 0) {
+      foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery);
+    } else {
+      foundMovies = await this._favoriteMovies.getAllMovies();
+    }
+
+    this._showFoundMovies(foundMovies);
+  }
+
+  _showFoundMovies(movies) {
+    let html;
+    console.log(movies.length);
+    if (movies.length > 0) {
+      html = movies.reduce(
+        (carry, movie) => carry.concat(`<li class="movie"><span class="movie__title">${movie.title || '-'}</span></li>`),
+        '',
+      );
+    } else {
+      html = '<div class="movies__not__found">Film tidak ditemukan</div>';
+    }
+
+    document.querySelector('.movies').innerHTML = html;
+
+    document.getElementById('movie-search-container')
+      .dispatchEvent(new Event('movies:searched:updated'));
+  }
+
+  get latestQuery() {
+    return this._latestQuery;
+  }
+}
+
+export default FavoriteMovieSearchPresenter;
